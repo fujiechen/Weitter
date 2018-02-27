7d950daaad29c8fe3ae82ea9abb260bb00f8d3fb
vagrant <vagrant@homestead>
2018-02-27 01:43:57 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,16 @@
 <html>
   <head>
     <title>@yield('title', 'Sample') - Laravel Small Project</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="containter">
+        <div class="col-md-offset-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </div>
   </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
 @section('content')
-    <h1>Home Page</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            Welcome to <a href="something">Sample App</a>'s Homepage!
+        </p>
+
+        <p>
+            This is the beginning.
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">Signup Now</a>
+        </p>
+    </div>
 @stop
