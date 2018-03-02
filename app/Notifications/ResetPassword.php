7a139a8ef928b3999bb734d413c74e376d3e3035
imaple7 <imaple7@gmail.com>
2018-03-02 06:34:29 +0000

    Add password resets with fake connection, send email to laravel.log

---
+++ app/Notifications/ResetPassword.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPassword extends Notification
+{
+    public $token;
+
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('This is Password Reset email, please click the following link: ')
+                    ->action('Password Reset', url(route('password.reset', $this->token, false)))
+                    ->line('If you are not doing this operation, please ignore this email!');
+    }
+}
